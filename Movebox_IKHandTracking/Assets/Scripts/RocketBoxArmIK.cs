629be3aae4571c50452282568a162b1a1677b114
Mar Gonzalez-Franco <margon@microsoft.com>
2020-10-15 22:26:30 -0700

    IK AND HANDTRACKING PROJECT

---
+++ Movebox_IKHandTracking/Assets/Scripts/RocketBoxArmIK.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class for the Arm IK for RocketBox avatar
+/// Attach this script to RocketBox avatar gameobject.
+/// </summary>
+public class RocketBoxArmIK : MonoBehaviour
+{
+    public bool enableLeftArm = true;
+    public bool enableRightArm = true;
+    public Transform effectorL;
+    public Transform effectorR;
+    public Transform HeadT;
+   
+
+    private RocketBoxArm armL;
+    private RocketBoxArm armR;
+    private Transform pelvis, head, eyes;
+
+    void Start()
+    {
+        Transform handBoneL = GameObject.Find("Bip01 L Hand").transform;
+        Transform handBoneR = GameObject.Find("Bip01 R Hand").transform;
+        pelvis = GameObject.Find("Bip01 Pelvis").transform;
+        head = GameObject.Find("Bip01 Head").transform;
+        eyes = GameObject.Find("Bip01 MMiddleEyebrow").transform;
+        armL = new RocketBoxArm(handBoneL, effectorL, new Vector3(180.0f, 0.0f, 180.0f));
+        armR = new RocketBoxArm(handBoneR, effectorR, new Vector3(180.0f, 0.0f, 0.0f));
+    }
+
+    void Update()
+    {
+        pelvis.position = HeadT.position - (eyes.position-pelvis.position);
+        head.rotation = HeadT.rotation * Quaternion.Euler(new Vector3(0.0f, -90.0f, -90.0f));
+     
+    }
+
+    void LateUpdate()
+    {
+        if (enableLeftArm)
+        {
+            armL.ApplyEffector(-90);
+        }
+        if (enableRightArm) {
+            armR.ApplyEffector(1);
+        }
+    }
+
+    /// <summary>
+    /// Class for RocketBox avatar's arm, including 3 bones.
+    /// </summary>
+    class RocketBoxArm
+    {
+        // only deal with IK when arm is complete (no bone missing).
+        public bool isComplete = false;
+        public Transform effector;
+
+        public Transform hand;
+        public Transform forearm;
+        public Transform upperarm;
+        public Vector3 offset;
+        public Vector3 iaPosition;
+        public Vector3 ibPosition;
+        public Vector3 icPosition;
+        Dictionary<string, Quaternion> originalAvatarAbsoluteRotations;
+
+        public RocketBoxArm(Transform iHand, Transform effectorX, Vector3 offsetX)
+        {
+            isComplete = false;
+
+            if (iHand == null)
+                return;
+
+            hand = iHand;
+            if (hand == null)
+                return;
+            forearm = hand.parent;
+            if (forearm == null)
+                return;
+
+            upperarm = forearm.parent;
+            if (upperarm == null)
+                return;
+
+            isComplete = true;
+
+            iaPosition = upperarm.position;
+            ibPosition = forearm.position;
+            icPosition = hand.position;
+            originalAvatarAbsoluteRotations = new Dictionary<string, Quaternion>();
+            originalAvatarAbsoluteRotations.Add(hand.name, hand.rotation);
+            originalAvatarAbsoluteRotations.Add(forearm.name, forearm.rotation);
+            originalAvatarAbsoluteRotations.Add(upperarm.name, upperarm.rotation);
+
+            offset = offsetX;
+
+            effector = effectorX;
+        }
+
+        /// <summary>
+        /// If the arm is detected complete, let the arm move along with hand (wrist) joint according to the effector's position and rotation. 
+        /// Call this method in your LateUpdate() 
+        /// </summary>
+        public void ApplyEffector(int deg)
+        {
+            if (isComplete)
+            {
+                Solve(deg);
+            }
+        }
+
+        /// <summary>
+        /// Returns the angle needed between v1 and v2 so that their extremities are
+        /// spaced with a specific length.
+        /// </summary>
+        /// <returns>The angle between v1 and v2.</returns>
+        /// <param name="aLen">The desired length between the extremities of v1 and v2.</param>
+        /// <param name="v1">First triangle edge.</param>
+        /// <param name="v2">Second triangle edge.</param>
+        private static float TriangleAngle(Vector3 hypotenuse, Vector3 v1, Vector3 v2)
+        {
+            float hLen = hypotenuse.magnitude;
+            float abLen = v1.magnitude;
+            float bcLen = v2.magnitude;
+            float c = Mathf.Clamp((abLen * abLen + bcLen * bcLen - hLen * hLen) / (abLen * bcLen * 2.0f), -1.0f, 1.0f);
+            //if((abLen * abLen + bcLen * bcLen - hLen * hLen)>0)
+            return Mathf.Acos(c);
+            //else
+            //  return 0;
+        }
+
+        private void Solve(int deg)
+        {
+
+            Quaternion aRotation = upperarm.rotation;
+            Quaternion bRotation = forearm.rotation;
+            Quaternion eRotation = effector.rotation * Quaternion.Euler(offset);
+
+            Vector3 aPosition = upperarm.position;
+            Vector3 bPosition = forearm.position;
+            Vector3 cPosition = hand.position;
+            Vector3 ePosition = effector.position;
+            
+
+            Vector3 ab = bPosition - aPosition;
+            Vector3 bc = cPosition - bPosition;
+            Vector3 ac = cPosition - aPosition;
+            Vector3 ae = ePosition - aPosition;
+            if (ae.magnitude > (icPosition-iaPosition).magnitude)
+            {
+                ae.Normalize();
+                ae *= (icPosition - iaPosition).magnitude;
+                return;
+            }
+
+            float abcAngle = TriangleAngle(ac, ab, bc);
+            float abeAngle = TriangleAngle(ae, ab, bc);
+
+            float angle = (abcAngle - abeAngle) * Mathf.Rad2Deg;
+            Vector3 axis = Vector3.Cross(ibPosition - iaPosition, icPosition - ibPosition).normalized;
+            
+            Quaternion fromToRotation = Quaternion.AngleAxis(angle, axis);
+
+            Quaternion worldQ = fromToRotation * bRotation;
+
+            forearm.rotation = worldQ;
+            FixRoll(forearm);
+
+            cPosition = hand.position;
+            ac = cPosition - aPosition;
+            Quaternion fromTo = Quaternion.FromToRotation(ac, ae);
+            upperarm.rotation = fromTo * aRotation;
+            FixRoll(upperarm);
+            hand.rotation = eRotation;
+            //FixRoll(hand);
+        }
+
+        private void FixRoll(Transform currentbone)
+        {
+            // Rotate the X axis from the direction in the original Avatar Absolute Rotation to the desired direction.
+
+            Vector3 Orig_Right = originalAvatarAbsoluteRotations[currentbone.name] * new Vector3(1, 0, 0);
+            Vector3 New_Right = currentbone.right;
+
+            Vector3 Axis = Vector3.Cross(Orig_Right, New_Right);
+            float angle = Vector3.Angle(Orig_Right, New_Right);
+
+            currentbone.rotation = originalAvatarAbsoluteRotations[currentbone.name];
+            //const float epsilon = 0.000000001f;
+           // if (angle >= epsilon)
+                currentbone.transform.Rotate(Axis, angle, Space.World);
+        }
+    }
+}
---
+++ Movebox_IKHandTracking/Assets/Scripts/RocketBoxOVRHandTracking.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using bid = OVRSkeleton.BoneId;
+
+/// <summary>
+/// Class for mapping Oculus hand tracking to RocketBox avatar hand
+/// Attach this script to RocketBox avatar gameobject.
+/// </summary>
+public class RocketBoxOVRHandTracking : MonoBehaviour
+{
+    public OVRSkeleton OVRSkeleton_L;
+    public OVRSkeleton OVRSkeleton_R;
+
+    private RocketBoxHand handL;
+    private RocketBoxHand handR;
+
+    void Start()
+    {
+        handL = new RocketBoxHand(RocketBoxHand.HandType.LeftHand);
+        handR = new RocketBoxHand(RocketBoxHand.HandType.RightHand, new Vector3(0.0f, 0.0f, 180.0f)); 
+    }
+
+    void LateUpdate()
+    {
+        handL.UpdateHand(OVRSkeleton_L);
+        handR.UpdateHand(OVRSkeleton_R);
+    }
+
+    /// <summary>
+    /// Class for RocketBox avatar's hand, including 5 fingers and 3 bones on each finger.
+    /// </summary>
+    class RocketBoxHand
+    {
+        public enum HandType
+        {
+            LeftHand,
+            RightHand
+        }
+
+        public HandType handType;
+        public FingerBone[,] fingerBones = new FingerBone[5,3];
+
+        /// <summary>
+        /// Auto detect finger bones, set offset.
+        /// </summary>
+        /// <param name="type">Left or right hand</param>
+        /// <param name="offset">Offset the finger rotation to make it works correctly. For right hand, it needs an offset of Vector3(0.0f, 0.0f, 180.0f).</param>
+        public RocketBoxHand(HandType type, Vector3 offset = new Vector3())
+        {
+            handType = type;
+            for (int fingerIndex = 0; fingerIndex <= fingerBones.GetUpperBound(0); fingerIndex++)
+            {
+                for(int fingerBoneIndex = 0; fingerBoneIndex <= fingerBones.GetUpperBound(1); fingerBoneIndex++)
+                {
+                    string name = "Finger" + fingerIndex + ((fingerBoneIndex == 0) ? "" : fingerBoneIndex.ToString());
+                    string namePrefix = "Bip01 " + ((type == HandType.LeftHand) ? "L " : "R ");
+                    Transform transform = GameObject.Find(namePrefix + name).transform;
+
+                    fingerBones[fingerIndex, fingerBoneIndex] = new FingerBone(name, transform, offset);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update the fingers of this hand, so they rotate as the OVR skeleton rotates. Call this in your program's LateUpdate()
+        /// </summary>
+        /// <param name="s"> OVRSkeleton, provided by Oculus Integration SDK. </param>
+        public void UpdateHand(OVRSkeleton s)
+        {
+            if (s == null || s.Bones.Count == 0)
+            {
+                return;
+            }
+
+            foreach (FingerBone f in fingerBones)
+            {
+                f.UpdateFinger(s); // if we want to change the offset for fingers, we can use UpdateFinger(s, newOffset).
+            }
+        }
+
+        override public string ToString()
+        {
+            string s = "";
+            s += $"_fingerBones.length: {fingerBones.Length}";
+            foreach (FingerBone f in fingerBones)
+            {
+                s += f.ToString();
+                s += "\n";
+            }
+            return s;
+        }
+
+        public class FingerBone
+        {
+            public string name;
+            public Transform transform;
+            public Vector3 offset;
+
+            public Dictionary<string, bid> boneMapping = new Dictionary<string, bid>
+            {
+                { "Finger0", bid.Hand_Thumb1 },
+                { "Finger01", bid.Hand_Thumb2 },
+                { "Finger02", bid.Hand_Thumb3 },
+
+                { "Finger1", bid.Hand_Index1 },
+                { "Finger11", bid.Hand_Index2 },
+                { "Finger12", bid.Hand_Index3 },
+
+                { "Finger2", bid.Hand_Middle1 },
+                { "Finger21", bid.Hand_Middle2 },
+                { "Finger22", bid.Hand_Middle3 },
+
+                { "Finger3", bid.Hand_Ring1 },
+                { "Finger31", bid.Hand_Ring2 },
+                { "Finger32", bid.Hand_Ring3 },
+
+                { "Finger4", bid.Hand_Pinky1 },
+                { "Finger41", bid.Hand_Pinky2 },
+                { "Finger42", bid.Hand_Pinky3 },
+            };
+
+            public FingerBone(string n, Transform t, Vector3 o)
+            {
+                name = n;
+                transform = t;
+                offset = o;
+            }
+
+            public void UpdateFinger(OVRSkeleton s)
+            {
+                if (s == null || s.Bones.Count == 0)
+                {
+                    return;
+                }
+
+                Transform tracking = s.Bones[(int)boneMapping[name]].Transform;
+
+                SetProperties(transform, tracking);
+            }
+
+            public void UpdateFinger(OVRSkeleton s, Vector3 newOffset)
+            {
+                offset = newOffset;
+                UpdateFinger(s);
+            }
+
+            private void SetProperties(Transform d1, Transform d2)
+            {
+                if (d1 == null || d2 == null)
+                {
+                    return;
+                }
+
+                //d1.position = d2.position; // Uncomment this if you want elastic fingers like alien ;)
+                d1.transform.rotation = d2.rotation;
+                d1.transform.rotation *= Quaternion.Euler(offset);
+            }
+
+            override public string ToString()
+            {
+                if (transform == null)
+                {
+                    return name + ": transform null";
+                }
+
+                return $"{name}: Position: {transform.position}; Rotation: {transform.rotation}";
+            }
+        }
+    }
+}
